da9e904d862ce48c5f29e2aee86a6d964a03ca56
Vitaliy Lyakh <fireflier87@gmail.com>
2022-08-22 16:33:08 +0300

    update task 03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,39 +2,35 @@ import throttle from 'lodash.throttle';
 
 const FORM_KEY = 'feedback-form-state';
 const form = document.querySelector('.feedback-form');
-
-let inputDataObj = {
-  email: '',
-  message: '',
-};
+const email = document.querySelector('input[name="email"]');
+const message = document.querySelector('textarea[name="message"]');
 
 form.addEventListener('input', throttle(onInput, 500));
 form.addEventListener('submit', onSubmit);
 
 getInput();
 
-function getInput() {
-  try {
-    const recievedData = localStorage.getItem(FORM_KEY);
-    if (!recievedData) return;
-  } catch (error) {
-    console.log('Get state error: ', error.message);
-  }
+function onInput() {
+  const inputDataObj = { email: email.value, message: message.value }
+  localStorage.setItem(FORM_KEY, JSON.stringify(inputDataObj));
 }
 
 function onSubmit(evt) {
   evt.preventDefault();
-  const { email, message } = evt.currentTarget.elements;
-  const formData = {
-    email: email.value,
-    message: message.value,
-  };
-  console.log(formData);
+  if (!email.value || !message.value) return alert('Fill in all fields');
+  console.log(JSON.parse(localStorage.getItem(FORM_KEY)));
   evt.currentTarget.reset();
   localStorage.removeItem(FORM_KEY);
 }
 
-function onInput(evt) {
-  inputDataObj[evt.target.name] = evt.target.value.trim();
-  localStorage.setItem(FORM_KEY, JSON.stringify(inputDataObj));
+function getInput() {
+  try {
+    const recievedData = localStorage.getItem(FORM_KEY);
+    if (recievedData) {
+      email.value = JSON.parse(recievedData).email
+      message.value = JSON.parse(recievedData).message
+    };
+  } catch (error) {
+    console.log('Get state error: ', error.message);
+  }
 }
